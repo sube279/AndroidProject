853d4e42dec0a6ae27536d759bc70e7fa6efc047
sube279 <Betrissu>
2015-10-11 20:57:34 +0200

    4 activités pour les fonctionnalités Ajouter le lien entre ImageButton et nouvelle activité

--- app/src/main/java/com/betrisey/suzanne/androidproject/Parametre.java
+++ app/src/main/java/com/betrisey/suzanne/androidproject/Parametre.java
@@ -1,48 +1,22 @@
 package com.betrisey.suzanne.androidproject;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-public class Home extends AppCompatActivity {
-
-    // Ouvre la fenetre donneur
-    public void openDonneur(View view) {
-        Intent intent = new Intent(this, Donneur.class);
-        startActivity(intent);
-    }
-
-    // Ouvre la fenetre intervention
-    public void openIntervention(View view) {
-        Intent intent = new Intent(this, Intervention.class);
-        startActivity(intent);
-    }
-
-    // Ouvre la fenetre banque du sang
-    public void openSang(View view) {
-        Intent intent = new Intent(this, Sang.class);
-        startActivity(intent);
-    }
-
-    // Ouvre la fenetre parametre
-    public void openParametre(View view) {
-        Intent intent = new Intent(this, Parametre.class);
-        startActivity(intent);
-    }
+public class Parametre extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_parametre);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home, menu);
+        getMenuInflater().inflate(R.menu.menu_parametre, menu);
         return true;
     }
 
