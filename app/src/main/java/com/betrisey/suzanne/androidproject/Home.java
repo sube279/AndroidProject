f1d841f222c30f87674d45a2a6922b1172c186e6
ChachaNy <nancy.zappellaz@students.hevs.ch>
2015-10-23 11:27:31 +0200

    AfficherDonneur modification affichage dans un TableLayout (dynamique)

--- app/src/main/java/com/betrisey/suzanne/androidproject/Home.java
+++ app/src/main/java/com/betrisey/suzanne/androidproject/Home.java
@@ -11,7 +11,7 @@ public class Home extends AppCompatActivity {
 
     // Ouvre la fenetre donneur
     public void openDonneur(View view) {
-        Intent intent = new Intent(this, Donneur.class);
+        Intent intent = new Intent(this, AfficherDonneur.class);
         startActivity(intent);
     }
 
