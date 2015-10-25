b5b3a433c8b3760c280c20528a20379088503f24
sube279 <Betrissu>
2015-10-25 20:15:14 +0100

    Navigation banque du sang

--- app/src/main/java/com/betrisey/suzanne/androidproject/AffichagePochetteSang.java
+++ app/src/main/java/com/betrisey/suzanne/androidproject/AffichagePochetteSang.java
@@ -1,7 +1,13 @@
 package com.betrisey.suzanne.androidproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewConfiguration;
+
+import java.lang.reflect.Field;
 
 public class AffichagePochetteSang extends AppCompatActivity {
 
@@ -9,5 +15,46 @@ public class AffichagePochetteSang extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_affichage_pochette_sang);
+
+        //ajouter icone a la barre d'action
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        //pour le menu (marche pas sur toutes les versions d'android sinon)
+        try {
+            ViewConfiguration config = ViewConfiguration.get(this);
+            Field menuKeyField = ViewConfiguration.class.getDeclaredField("sHasPermanentMenuKey");
+            if(menuKeyField != null) {
+                menuKeyField.setAccessible(true);
+                menuKeyField.setBoolean(config, false);
+            }
+        } catch (Exception ex) {
+            // Ignore
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_pochette, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()) {
+
+            case R.id.action_statut:
+                Intent intent2 = new Intent(this, ChoixStatut.class);
+                startActivity(intent2);
+                return true;
+            case R.id.action_transfert:
+                Intent intent = new Intent(this, Transfert.class);
+                startActivity(intent);
+                return true;
+            }
+        return (super.onOptionsItemSelected(item));
     }
 }
--- app/src/main/java/com/betrisey/suzanne/androidproject/ChoixStatut.java
+++ app/src/main/java/com/betrisey/suzanne/androidproject/ChoixStatut.java
@@ -1,7 +1,12 @@
 package com.betrisey.suzanne.androidproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewConfiguration;
+
+import java.lang.reflect.Field;
 
 public class ChoixStatut extends AppCompatActivity {
 
@@ -9,5 +14,31 @@ public class ChoixStatut extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choix_statut);
+
+        //ajouter icone a la barre d'action
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+
+        //pour le menu (marche pas sur toutes les versions d'android sinon)
+        try {
+            ViewConfiguration config = ViewConfiguration.get(this);
+            Field menuKeyField = ViewConfiguration.class.getDeclaredField("sHasPermanentMenuKey");
+            if(menuKeyField != null) {
+                menuKeyField.setAccessible(true);
+                menuKeyField.setBoolean(config, false);
+            }
+        } catch (Exception ex) {
+            // Ignore
+        }
+    }
+
+    public void buttonAnnuler(View view) {
+        Intent intent = new Intent(this, AffichagePochetteSang.class);
+        startActivity(intent);
+    }
+
+    public void buttonOk(View view) {
+        Intent intent = new Intent(this, AffichagePochetteSang.class);
+        startActivity(intent);
     }
 }
