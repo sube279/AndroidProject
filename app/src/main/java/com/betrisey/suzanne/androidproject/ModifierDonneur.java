8dfd816f85f23195241d65822a01a6a3ee865f9c
ChachaNy <nancy.zappellaz@students.hevs.ch>
2015-11-10 18:11:32 +0100

    ModifierDonneur (RadioButton + Spinner)

--- app/src/main/java/com/betrisey/suzanne/androidproject/ModifierDonneur.java
+++ app/src/main/java/com/betrisey/suzanne/androidproject/ModifierDonneur.java
@@ -6,9 +6,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -72,6 +77,8 @@ public class ModifierDonneur extends AppCompatActivity {
         TableLayout table=(TableLayout) findViewById(R.id.tableLayout);
         TableRow row;
         TextView tv1,tv2;
+        Spinner spinner;
+        String[] regions = { "Sion", "Sierre", "Montana", "Brigue", "Viège", "Monthey", "Martigny"};
 
         TableRow.LayoutParams params = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT);
         params.width=0;
@@ -86,18 +93,43 @@ public class ModifierDonneur extends AppCompatActivity {
             tv1.setText(info[i]); // ajout du texte
             tv1.setLayoutParams(params);
 
-            tv2 = new EditText(this); // création cellule
-            tv2.setId(i);
-            tv2.setText(donneur.get(i)); // ajout du texte;
-            tv2.setLayoutParams(params);
-
             //ajout des cellules à la ligne
             row.addView(tv1);
-            row.addView(tv2);
 
+            if(i==2){
+                RadioGroup radioGroup = new RadioGroup(this);
+                RadioButton fem = new RadioButton(this);
+                fem.setText("Féminin");
+                RadioButton masc = new RadioButton(this);
+                masc.setText("Masculin");
+
+                //Rajouter le test pour cocher la bonne sélection par défaut
+
+                radioGroup.addView(fem);
+                radioGroup.addView(masc);
+                radioGroup.setLayoutParams(params);
+                row.addView(radioGroup);
+
+            } else if (i == 7){
+                spinner = new Spinner(this);
+                ArrayAdapter<Integer> stringArrayAdapter=
+                        new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, regions);
+                spinner.setAdapter(stringArrayAdapter);
+
+                //Rajouter le test pour sélectionner la bonne région par défaut
+
+                spinner.setLayoutParams(params);
+                row.addView(spinner);
+            }else{
+                tv2 = new EditText(this); // création cellule
+                tv2.setId(i);
+                tv2.setText(donneur.get(i)); // ajout du texte;
+                tv2.setLayoutParams(params);
+
+                row.addView(tv2);
+            }
             //ajout de la ligne au tableau
             table.addView(row);
-
         }
     }
 
--- app/src/main/java/com/betrisey/suzanne/androidproject/SupprimerIntervention.java
+++ app/src/main/java/com/betrisey/suzanne/androidproject/SupprimerIntervention.java
@@ -14,6 +14,7 @@ import android.widget.BaseAdapter;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.ListView;
+import android.widget.TableLayout;
 import android.widget.TextView;
 
 import java.lang.reflect.Field;
